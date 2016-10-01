9b20e1fe0bf728941b3872d9b7825c07b0b26de8
Charles M. Griffin <cmg1229@Masseys-MacBook-Pro.local>
2016-10-01 12:39:36 -0500

    Added DB code

--- foow/models.py
+++ foow/models.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+def defaultTitle():
+	return {"blog_title", "Post Title"}
+
+def defaultText():
+    return {"blog_text", "Blog Text"}
+
+class BlogPost(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    blog_title = models.CharField(max_length=30,default=defaultTitle)
+    blog_text = models.CharField(max_length=2000,default=defaultText)
+    pub_date = models.DateTimeField(default=timezone.now())
+    picture_location = models.CharField(max_length=50, null=True)
--- foow/views.py
+++ foow/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from foow.models import BlogPost
 
 # Create your views here.
+def index(request):
+	return render(request, 'foow/index.html')
+
+def blogpost(request, id):
+	b = BlogPost.objects.get(post_id=id)
+	return render(request, 'foow/detail.html', {'BlogPost':b})
