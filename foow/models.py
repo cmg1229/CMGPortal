0d21d10e3dbefb663d12e68d361b964e0b6ad7cf
Charles M. Griffin <cmg1229@Masseys-MacBook-Pro.local>
2016-10-24 15:17:51 +0200

    October 24 2016

--- foow/models.py
+++ foow/models.py
@@ -13,7 +13,8 @@ def defaultText():
 
 class BlogPost(models.Model):
     post_id = models.AutoField(primary_key=True)
-    blog_title = models.CharField(max_length=30,default=defaultTitle)
-    blog_text = models.CharField(max_length=2000,default=defaultText)
+    blog_title = models.CharField(max_length=30,default='Default Title')
+    blog_subtitle = models.CharField(max_length=30, default='Default SubTitle')
+    blog_text = models.CharField(max_length=8000,default='Default Text')
     pub_date = models.DateTimeField(default=timezone.now())
     picture_location = models.CharField(max_length=50, null=True)
